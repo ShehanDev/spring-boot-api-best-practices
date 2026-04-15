72a2079c7f71bd824194ce293bfcca211da2c093
Shehan Mallawaarachchi <78250147+ShehanDev@users.noreply.github.com>
2026-04-15 15:56:31 +0530

    docs: enhance EmployeeController with OpenAPI annotations for better API documentation

--- src/main/java/com/shehan/workflow_service/controller/EmployeeController.java
+++ src/main/java/com/shehan/workflow_service/controller/EmployeeController.java
@@ -2,6 +2,12 @@ package com.shehan.workflow_service.controller;
 import com.shehan.workflow_service.dto.EmployeeDto;
 import com.shehan.workflow_service.model.Employee;
 import com.shehan.workflow_service.service.EmployeeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,14 +22,26 @@ import java.util.List;
 @RestController
 //Base URL for all endpoints in this controller
 @RequestMapping("v1/api/employees")
+@Tag(name = "Employee Management API", description = "API for managing employee data")
 public class EmployeeController {
 
 
     private EmployeeService  employeeService;
 
+
+
+
+    @Operation(summary = "Create a new employee", description = "Creates a new employee with the provided data")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "employee created successfully",
+                    content = @Content(schema = @Schema(implementation = EmployeeDto.class))),
+            @ApiResponse(responseCode = "400", description = "Invalid request data",
+                    content = @Content(schema = @Schema()))
+    })
     //This method will handle HTTP POST requests to create a new employee
     @PostMapping
-    //
+    //The @RequestBody annotation indicates that the employee data
+    // will be sent in the request body as JSON and will be deserialized into an EmployeeDto object.
     public ResponseEntity<EmployeeDto> createEmployee(@RequestBody EmployeeDto employeeDto){
         EmployeeDto savedEmplyee = employeeService.createEmployee(employeeDto);
         return  new  ResponseEntity<>(savedEmplyee, HttpStatus.CREATED);
@@ -31,6 +49,13 @@ public class EmployeeController {
 
 
 
+    @Operation(summary = "Get employee by ID", description = "Retrieves an employee by their ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Employee found",
+                    content = @Content(schema = @Schema(implementation = EmployeeDto.class))),
+            @ApiResponse(responseCode = "404", description = "Employee not found",
+                    content = @Content(schema = @Schema()))
+    })
     //This method will handle HTTP GET requests to retrieve an employee by their ID
     @GetMapping("{id}")
     public ResponseEntity<EmployeeDto> getEmployeeById(@PathVariable("id") Long employeeId){
@@ -39,6 +64,12 @@ public class EmployeeController {
 
     }
 
+
+    @Operation(summary="Get all employees ",description = "Retrieves a list of all employees")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Users retrieved successfully",
+                    content = @Content(schema = @Schema(implementation = EmployeeDto.class)))
+    })
     //Get all employees list
     @GetMapping
     public ResponseEntity<List<EmployeeDto>> getAllEmployees(){
@@ -47,6 +78,13 @@ public class EmployeeController {
     };
 
 
+    @Operation(summary = "Update an existing employee", description = "Updates an existing employee with the provided data")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Employee updated successfully",
+                    content = @Content(schema = @Schema(implementation = EmployeeDto.class))),
+            @ApiResponse(responseCode = "404", description = "Employee not found",
+                    content = @Content(schema = @Schema()))
+    })
     //update Employee
     @PutMapping("{id}")
     public ResponseEntity<EmployeeDto> updateEmployee(@PathVariable("id") Long employeeId ,@RequestBody EmployeeDto updatedEmpData){
@@ -54,6 +92,12 @@ public class EmployeeController {
         return ResponseEntity.ok(updatedEmployee);
     }
 
+    @Operation(summary = "Delete an employee", description = "Deletes an employee by their ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "User deleted successfully"),
+            @ApiResponse(responseCode = "404", description = "User not found",
+                    content = @Content(schema = @Schema()))
+    })
     //Delete employee by id
     @DeleteMapping("{id}")
     public ResponseEntity<String> deleteEmployee(@PathVariable("id") Long employeeId){
